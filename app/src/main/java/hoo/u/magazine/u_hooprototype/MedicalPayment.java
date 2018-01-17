b6fd103c27ac72d4ecc32e49514a013c447ceff9
ItsYaBoiBry <bryanlow987@gmail.com>
2018-01-17 12:48:40 +0800

    fixed Medical Payment Page

--- app/src/main/java/hoo/u/magazine/u_hooprototype/MedicalPayment.java
+++ app/src/main/java/hoo/u/magazine/u_hooprototype/MedicalPayment.java
@@ -3,6 +3,8 @@ package hoo.u.magazine.u_hooprototype;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
@@ -12,15 +14,26 @@ import de.hdodenhof.circleimageview.CircleImageView;
 public class MedicalPayment extends AppCompatActivity {
     CircleImageView docImg;
     TextView docName, docPrice;
+    Button btnBackToListing;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_payment);
         docImg = findViewById(R.id.docImg);
         docName = findViewById(R.id.docName);
         docPrice = findViewById(R.id.docPrice);
+        btnBackToListing = findViewById(R.id.btnBackToListing);
 
+
+        btnBackToListing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MedicalPayment.this, ActivityMedicalConsult.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         Intent intent = getIntent();
         ClassDoctor doctor = (ClassDoctor) intent.getSerializableExtra("doc");
         Picasso.with(getApplicationContext()).load(doctor.getDocImage()).into(docImg);
